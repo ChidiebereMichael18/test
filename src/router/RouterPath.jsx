6784b6c0899b17ed1af2a9eeb1ffdcb1d51de94c
codewithmike-jpg <mikeyxofficial1234@gmail.com>
2025-08-26 04:14:25 +0100

    fixed routes

--- src/router/RouterPath.jsx
+++ src/router/RouterPath.jsx
@@ -6,10 +6,11 @@ import DashboardLayout from '../components/layout/DashboardLayout';
 const RouterPath = () => {
   return (
     <Routes>
-      <Route index element={<DashboardLayout />} />
-      <Route path="/dashboard" element={<DashboardLayout />}>
+      <Route path="/" element={<DashboardLayout />}>
         <Route index element={<Home />} />
+        <Route path="dashboard" element={<Home />} />
       </Route>
+
     </Routes>
   );
 };
